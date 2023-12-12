50be329f402d7226facfa6b7960e1896df1de096
tuchuan <imtuchuan@gmail.com>
2023-12-12 20:45:13 +0800

    feat: 容器化支持

--- extend_tools/web_healthz.sh
+++ extend_tools/web_healthz.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-source /root/.bkrc
-source /data/install/utils.fc
-workon bkmonitorv3-monitor
-./bin/manage.sh healthz $1 $2 $3 $4
+
+if [ $1 = "-kube" ];then
+  pod_name=$(kubectl get pods -n blueking | grep monitor-alarm-access | awk '$3 ~ /Running/ {print $1}' | head -1)
+  kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py healthz $2 $3 $4 $5
+else
+  source /root/.bkrc
+  source /data/install/utils.fc
+  workon bkmonitorv3-monitor
+  ./bin/manage.sh healthz $1 $2 $3 $4
+fi
