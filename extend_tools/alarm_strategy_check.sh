caa1d12372c1a79ed6efad1da1105523b01c7ae7
tuchuan <imtuchuan@gmail.com>
2023-12-14 14:24:23 +0800

    fix: 修复参数下标错误

--- extend_tools/alarm_strategy_check.sh
+++ extend_tools/alarm_strategy_check.sh
@@ -3,8 +3,8 @@
 if [ $1 = "-kube" ];then
   pod_name=$(kubectl get pods -n blueking | grep monitor-alarm-access | awk '$3 ~ /Running/ {print $1}' | head -1)
   echo "pod name is: $pod_name"
-  echo "command is: kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}"
-  kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
+  echo "command is: kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py strategy_check $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}"
+  kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py strategy_check $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
 else
   source /root/.bkrc
   source /data/install/utils.fc
