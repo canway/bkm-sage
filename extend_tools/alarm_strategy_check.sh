bb88196ddbcb4966e631a22d73936c581cb28519
tuchuan <imtuchuan@gmail.com>
2023-12-14 11:04:55 +0800

    fix: 修复 bash 中变量超过 10 导致的问题

--- extend_tools/alarm_strategy_check.sh
+++ extend_tools/alarm_strategy_check.sh
@@ -2,10 +2,13 @@
 
 if [ $1 = "-kube" ];then
   pod_name=$(kubectl get pods -n blueking | grep monitor-alarm-access | awk '$3 ~ /Running/ {print $1}' | head -1)
-  kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11
+  echo "pod name is: $pod_name"
+  echo "command is: kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}"
+  kubectl exec -it $pod_name -n blueking -- /app/venv/bin/python manage.py strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
 else
   source /root/.bkrc
   source /data/install/utils.fc
   workon bkmonitorv3-monitor
-  ./bin/manage.sh strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11
+  echo "command is：./bin/manage.sh strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}"
+  ./bin/manage.sh strategy_check $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
 fi
